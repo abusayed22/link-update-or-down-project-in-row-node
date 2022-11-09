376f52fed7fa985a50c5a34619d325a41910486f
abusayed22 <abusayed.in22@gmail.com>
2022-11-09 10:33:07 +0600

    'ok'

--- handlers/userHandler.js
+++ handlers/userHandler.js
@@ -23,10 +23,10 @@ handler.userHandler = (requestProparties, callBack) => {
 handler._users = {}
 
 handler._users.post = (requestProparties, callBack) => {
-    const name = typeof (requestProparties.body.name === 'string' && requestProparties.body.name.trim().length > 0) ? requestProparties.body.name : false;
-    const phone = typeof (requestProparties.body.phone === 'string' && requestProparties.body.phone.trim().length === 11) ? requestProparties.body.phone : false;
-    const addreess = typeof (requestProparties.body.addreess === 'string' && requestProparties.body.addreess.trim().length > 0) ? requestProparties.body.addreess : false;
-    const password = typeof (requestProparties.body.password)
+    const name = typeof requestProparties.body.name === 'string' && requestProparties.body.name.trim().length > 0 ? requestProparties.body.name : false;
+    const phone = typeof requestProparties.body.phone === 'string' && requestProparties.body.phone.trim().length === 11 ? requestProparties.body.phone : false;
+    const addreess = typeof requestProparties.body.addreess === 'string' && requestProparties.body.addreess.trim().length > 0 ? requestProparties.body.addreess : false;
+    const password = typeof requestProparties.body.password === 'string' && requestProparties.body.password.trim().length > 0 ? requestProparties.body.password : false;
 }
 
 handler._users.get = (requestProparties, callBack) => {
