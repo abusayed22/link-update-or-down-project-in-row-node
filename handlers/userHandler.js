3c400d38e21fa324ea14485432517c21ba9da677
abusayed22 <abusayed.in22@gmail.com>
2022-11-10 10:18:26 +0600

    crud opparetion done

--- handlers/userHandler.js
+++ handlers/userHandler.js
@@ -8,7 +8,7 @@
 // defendencies
 const data = require("../lib/data");
 const { hashing } = require("../helpers/utilities/utilities");
-const jsonformat = require('../helpers/utilities/utilities')
+const { jsonformat } = require('../helpers/utilities/utilities')
 
 // scaffoding - object module
 const handler = {};
@@ -23,7 +23,7 @@ handler.userHandler = (requestProparties, callBack) => {
     }
 };
 
-// private
+// service _ use
 handler._users = {};
 
 handler._users.post = (requestProparties, callBack) => {
@@ -76,23 +76,24 @@ handler._users.post = (requestProparties, callBack) => {
     }
 };
 
+//@:TODO get method not working
 handler._users.get = (requestProparties, callBack) => {
     // check the specific identier of phone number
     const phone =
-        typeof requestProparties.queryString.phone === "string" &&
+        typeof requestProparties.queryString.phone === 'string' &&
             requestProparties.queryString.phone.trim().length === 11
-            ? requestProparties.body.phone
+            ? requestProparties.queryString.phone
             : false;
 
     if (phone) {
-        data.read('user', phone, (err, user) => {
-            const userData = { ...jsonformat(user) }
+        data.read('users', phone, (err, u) => {
+            const userData = { ...jsonformat(u) }
             if (!err && userData) {
                 delete userData.password;
-                callBack(200,  userData )
+                callBack(200, userData);
             } else {
                 callBack(404, {
-                    error: 'Requested user was not found!'
+                    error: 'Requested user was not found!!!'
                 })
             }
         })
@@ -103,8 +104,100 @@ handler._users.get = (requestProparties, callBack) => {
     }
 };
 
-handler._users.put = (requestProparties, callBack) => { };
+handler._users.put = (requestProparties, callBack) => {
+    const name =
+        typeof requestProparties.body.name === "string" &&
+            requestProparties.body.name.trim().length > 0
+            ? requestProparties.body.name
+            : false;
+    const phone =
+        typeof requestProparties.body.phone === "string" &&
+            requestProparties.body.phone.trim().length === 11
+            ? requestProparties.body.phone
+            : false;
+    const addreess =
+        typeof requestProparties.body.addreess === "string" &&
+            requestProparties.body.addreess.trim().length > 0
+            ? requestProparties.body.addreess
+            : false;
+    const password =
+        typeof requestProparties.body.password === "string" &&
+            requestProparties.body.password.trim().length > 0
+            ? requestProparties.body.password
+            : false;
 
-handler._users.delete = (requestProparties, callBack) => { };
+    if (phone) {
+        if (name || addreess || password) {
+            data.read('users', phone, (err, user) => {
+                const userData = { ...jsonformat(user) }
+                /* they are same problem @userData undefind */
+                if (!err && userData) {
+                    if (name) {
+                        userData.name = name
+                    }
+                    if (addreess) {
+                        userData.addreess = addreess
+                    }
+                    if (password) {
+                        userData.password = hashing(password)
+                    }
+
+                    // update to database
+                    data.update('users', phone, userData, (err2) => {
+                        if (!err2) {
+                            callBack(200, userData)
+                        } else {
+                            callback(500, {
+                                error: 'There was a problem in the server side!',
+                            });
+                        }
+                    })
+                } else {
+                    callBack(400, {
+                        'error': 'there was a problem in sever side!!!'
+                    })
+                }
+            })
+        } else {
+            callBack(400, {
+                'error': 'your requested have an error!'
+            })
+        }
+    } else {
+        callBack(400, {
+            "error": "there was a problem in sever side!"
+        })
+    }
+};
+
+handler._users.delete = (requestProparties, callBack) => {
+    const phone =
+    typeof requestProparties.queryString.phone === "string" &&
+        requestProparties.queryString.phone.trim().length === 11
+        ? requestProparties.queryString.phone
+        : false;
+
+    if(phone) {
+        data.read('users',phone, (err) => {
+            if(!err) {
+                data.delete('users',phone, (err2) => {
+                    if(!err2) {
+                        callBack(200, {
+                            'message': 'deleted was successfully!'
+                        })
+                    } else {
+                        callBack(404,{
+                            'error': 'your delete requested was not found!'
+                        })
+                    }
+                })
+            } else  {
+                callBack(404,{
+                    'error': 'your delete requested was not found!!!'
+                })
+            }
+        })
+    }
+};
 
 module.exports = handler;
