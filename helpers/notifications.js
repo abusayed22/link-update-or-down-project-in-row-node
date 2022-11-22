f0c2ff5460d5318cb5b756efbe0041c7e9a11f77
abusayed22 <abusayed.in22@gmail.com>
2022-11-22 21:52:50 +0600

    some code are due of notifications porgram

--- helpers/notifications.js
+++ helpers/notifications.js
@@ -38,11 +38,25 @@ notifications.sendTwilioSms = (phone, msg, callback) => {
             hostName: api.twilio.com,
             method: POST,
             path: `2010-04-01/Accounts/${twilio.accountSid}/Messages.json`,
-            auth: `${twilio.accountSid}:${twilio.authToken}`
+            auth: `${twilio.accountSid}:${twilio.authToken}`,
+            headers: {
+                'Content-type': 'applications/x-www-from-urlencoded'
+            }
         };
 
-        // instantiate the request object
 
+        // instantiate the request object
+        const req = https.request(requestObject, (res) => {
+            // initiate response code
+            const statusCode = res.statusCode
+
+            // checking code is true & 
+            if(statusCode === 200 || statusCode === 201) {
+                callback(false)
+            } else {
+                callback(`status code was returnd ${statusCode}`)
+            }
+        })
     }else {
         callback('Given parameters wherer missing or invalid!')
     }
